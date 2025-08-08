700fcf9b163fc58a949b99d3d30c1d391ceed223
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-08-08 20:37:31 +0300

    do custom rule on specific field using callback function

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -16,14 +16,21 @@ class Category extends Model
     ];
     public static function rules($id = 0){
         return [
-            'name' => "required|string|min:3|max:255|unique:categories,name,$id",
+            'name' => ["required" ,"string","min:3","max:255","unique:categories,name,$id",
+            function($attribute, $value, $fails){
+                if(strtolower($value) == "laravel"){
+                    $fails('The name field must not be "laravel"');
+                }
+            }
+        ],
             'parent_id' => 'nullable|int|exists:categories,id',
             'image' =>[
                 'image',
                 'mimes:jpg,jpeg,png',
                 'dimensions:min_width=100,min_height=100'
             ],
-            'status' => "required|in:active,archived"
+            'status' => "required|in:active,archived",
+
         ];
     }
 }
