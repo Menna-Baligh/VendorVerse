fa5cb719cc95104a296077f71697f67a1bc0c452
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-08-28 16:47:54 +0300

    Add store_id to users table for assigning shop owners, keep null for admins

---
+++ database/migrations/2025_08_28_134054_add_store_id_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Store;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(Store::class)->after('phone_number')->nullable()->constrained()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropConstrainedForeignIdFor(Store::class);
+        });
+    }
+};
