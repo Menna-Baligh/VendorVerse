8d2d83db8aa75ab1acaf4288277512bd8927e40b
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-08-30 21:26:04 +0300

    Add Profile model and migration, establish one-to-one relationship with User

---
+++ database/migrations/2025_08_30_181311_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->foreignIdFor(User::class)->primary()->constrained()->onDelete('cascade');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birthday')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->string('street_address')->nullable();
+            $table->string('city');
+            $table->string('state')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->char('country',2);
+            $table->char('locale',2)->default('en');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
